541f258b53771b8c86de147b247e947e7e4a8b7a
aharownjackson <aharownmjackson@gmail.com>
2022-11-28 16:20:16 -0600

    finished practice exercise

--- solutions.js
+++ solutions.js
@@ -21,3 +21,56 @@
  * concat
  */
 
+// 1
+function isTrue(input){
+    return input === true;
+}
+
+// 2
+function isFalse(input){
+    return input === false;
+}
+
+// 3
+function not(input){
+    return !input;
+}
+
+// 4
+function addOne(num){
+    let input = parseFloat(num);
+    return input + 1;
+}
+
+// 5
+function isEven(num){
+    let number = parseFloat(num);
+    return number % 2 == 0;
+}
+
+// 6
+function isIdentical(input1, input2){
+    return input1 === input2;
+}
+
+// 7
+function isEqual(input1, input2){
+    return input1 == input2;
+}
+
+// 8
+function or(input1, input2){
+    return input1 || input2;
+}
+
+// 9
+function and(input1, input2){
+    return input1 && input2;
+}
+
+// 10
+function concat(input1, input2){
+    let input1string = input1.toString();
+    let input2string = input2.toString();
+    return input1string + input2string;
+}
